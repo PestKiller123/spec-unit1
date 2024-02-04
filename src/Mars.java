a206aae99f5fe6d21a4a4401bcfe2853a46fa4ec
PestKiller123 <gwpixs@gmail.com>
2024-02-04 09:49:55 -0700

    finished basic Mars.java added GuessingGame.java

--- src/Mars.java
+++ src/Mars.java
@@ -1,10 +1,10 @@
 
 public class Mars {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String colonyName = "X-Force";
         int shipPopulation = 300;
         double meals = 4000.00;
-        boolean landing = true;
+        boolean landing = landingCheck(12);
         meals -= (shipPopulation * 0.75);
         meals -= (shipPopulation * 0.75);
         System.out.println(meals);
@@ -16,5 +16,21 @@ public class Mars {
         } else {
             System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
         }
+    }
+    public static boolean landingCheck(int minutesLeft) throws InterruptedException {
+        for (int minutes = 0; minutes <= minutesLeft; minutes++) {
+            if (((minutes % 3) == 0) && ((minutes % 2) == 0)) {
+                System.out.println("Keep Center");
+            } else if ((minutes % 3) == 0) {
+                System.out.println("Left");
+            } else if ((minutes % 2) == 0) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
+        System.out.println("Landed");
+        return false;
     }
 }
